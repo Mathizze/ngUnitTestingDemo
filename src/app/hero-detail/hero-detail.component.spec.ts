7632da33be674ec1814b359d8e5f8761d1bc170d
mathijs.hardeman <mathijs.hardeman@MC-C2VVBG2>
2019-04-19 16:21:31 +0200

    advanced topics

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { TestBed, ComponentFixture, fakeAsync, tick, flush, async } from "@angular/core/testing";
 import { HeroDetailComponent } from "./hero-detail.component";
 import { ActivatedRoute } from "@angular/router";
 import { HeroService } from "../hero.service";
@@ -34,5 +34,22 @@ describe('HeroDetailComponent', () => {
   it('should render hero name in  a h2 tag', () => {
     fixture.detectChanges();
     expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
-  })
+  });
+
+  it('should call updateHero when save is called', fakeAsync(() => {
+    mockHeroService.updateHero.and.returnValue(of({}));
+    fixture.detectChanges();
+    fixture.componentInstance.save();
+    flush();
+    expect(mockHeroService.updateHero).toHaveBeenCalled();
+  }));
+
+  //it('should call updateHero when save is called', async(() => {
+  //  mockHeroService.updateHero.and.returnValue(of({}));
+  //  fixture.detectChanges();
+  //  fixture.componentInstance.save();
+  //  fixture.whenStable().then(() => {
+  //    expect(mockHeroService.updateHero).toHaveBeenCalled();
+  //  });
+  //}));
 });
